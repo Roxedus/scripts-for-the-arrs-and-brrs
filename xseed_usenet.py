a9ab4385c5e8d89ebf0d9e35ea7768741e9d66f3
Roxedus <me@roxedus.dev>
2023-05-20 17:03:37 +0200

    updates

--- xseed_usenet.py
+++ xseed_usenet.py
@@ -7,23 +7,39 @@ Description: Script to automate hardlinking and cross-seeding for Usenet downloa
 
 import os
 import requests
+import sys
 from pathlib import Path
 import argparse
 
 # settings
 base_path = "/home/user/Downloads/complete/"  # replace with the path where your completed Usenet downloads are stored
-cross_base_url = (
-    "http://127.0.0.1:2468"  # replace with the base URL of your cross-seed instance
-)
+cross_base_url = "http://127.0.0.1:2468"  # replace with the base URL of your cross-seed instance
 dest_path = "/home/user/torrents/qbittorrent/usenet/"  # replace with the path where you want to create hardlinks
 unattended = False  # set to True to run without user interaction
+recursive = True # set to False to run without traversing folders
+
+if os.environ.get("SAB_COMPLETE_DIR", False):
+    base_path = os.environ.get("SAB_COMPLETE_DIR")
+    unattended = True
+    POSTPROCESS_SUCCESS = 0
+    POSTPROCESS_ERROR = 1
+
+elif os.environ.get("NZBPP_DIRECTORY", False):
+    base_path = os.environ.get("NZBPP_DIRECTORY")
+    unattended = True
+    POSTPROCESS_SUCCESS = 93
+    POSTPROCESS_ERROR = 94
+
+else:
+    POSTPROCESS_SUCCESS = 0
+    POSTPROCESS_ERROR = 1
 
 
 def find_files(path: Path, extensions: tuple):
     for entry in path.iterdir():
         if entry.is_file() and entry.suffix in extensions:
             yield entry
-        elif entry.is_dir():
+        elif entry.is_dir() and recursive:
             yield from find_files(entry, extensions)
 
 
@@ -40,8 +56,10 @@ def send_webhook(url: str, directory_path: str):
     response = requests.post(url + "/api/webhook", data=data)
     if response.status_code == 204:
         print("Trigger sent successfully.")
+        sys.exit(POSTPROCESS_SUCCESS)
     else:
         print("Trigger failed.")
+        sys.exit(POSTPROCESS_ERROR)
 
 
 def user_prompt(question, default="no"):
@@ -62,11 +80,15 @@ def user_prompt(question, default="no"):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process some files.")
     parser.add_argument(
-        "--unattended", action="store_true", help="run script without user interaction"
+        "--recursive", action="store", help="run script recursively in 'base_path'"
     )
-    args = parser.parse_args()
+    parser.add_argument(
+        "--unattended", action="store", help="run script without user interaction"
+    )
+    args, unknown = parser.parse_known_args()
 
     unattended = unattended or args.unattended
+    recursive = recursive or args.recursive
 
     files = list(find_files(Path(base_path), (".mkv", ".mp4")))
     print(f"{len(files)} non-hardlinked movies found.")
@@ -85,5 +107,7 @@ if __name__ == "__main__":
     ):
         print(f"Triggering cross-seed search in {dest_path}")
         send_webhook(cross_base_url, dest_path)
+        sys.exit(POSTPROCESS_SUCCESS)
     else:
         print("Not triggering a cross-seed search")
+        sys.exit(POSTPROCESS_SUCCESS)
